62335153d45dfbbee3a4e7a3c8fadcf668b588f6
Liang Cheng <liang.cheng@kl2tech.com>
2024-12-19 15:59:53 -0500

    update mean motion

--- tletools/tle.py
+++ tletools/tle.py
@@ -301,7 +301,7 @@ class TLE:
             # M=orbit.n * (24 * 60 * 60 * u.s) << u.deg, # mean motion
             # n=orbit.nu.to(u.deg).value,
             M = orbit.nu.to(u.deg).value, # orbit.nu.to(u.deg).value, # mean anomaly
-            n = orbit.n, # mean motion
+            n = orbit.n * (24 * 60 * 60 * u.s) << u.deg / (360 * u.deg), # mean motion
             rev_num=rev_num
         )
 
